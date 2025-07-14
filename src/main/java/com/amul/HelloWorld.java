4583cd470c7b5f7aaba080e64c2395c6e412d0aa
Cloud User <2021wc86135@wilp.bits-pilani.ac.in>
2025-07-15 00:32:54 +0530

    Fixed file name to match public class

--- src/main/java/com/amul/HelloWorld.java
+++ src/main/java/com/amul/HelloWorld.java
@@ -2,8 +2,9 @@ package com.amul;
  
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI!");
+        System.out.println("Hello from CI pipeline!");
     }
 }
+
 
 
